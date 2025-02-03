8c79dfbd9b52d924827f959d792a7a145f78c1d0
Brumertz <nbrandon165@gmail.com>
2025-02-04 00:00:32 +0800

    Second commit
    nav-development

--- 3D-Portfolio/tailwind.config.cjs
+++ 3D-Portfolio/tailwind.config.cjs
@@ -1,15 +1,14 @@
-/** @type {import('tailwindcss').Config} */  
-module.exports = {  
+/** @type {import('tailwindcss/postcss').Config} */  
+export default {  
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],  
   theme: {  
     extend: {  
       colors: {  
-        primary: "#050816",  
-        secondary: "#aaa6c3",  
-        tertiary: "#151030",  
-        "black-100": "#100d25",  
-        "black-200": "#090325",  
-        "white-100": "#f3f3f3",  
+        secondary: "[#aaa6c3]",  
+        tertiary: "[#151030]",  
+        "black-100": "[#100d25]",  
+        "black-200": "[#09032]",  
+        "white-100": "[#f3f3f3]",  
       },  
       boxShadow: {  
         card: "0px 35px 120px -15px #211e35",  
