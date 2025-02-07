1ee3d85953f2ece246ff2addfda33b66b9ec6181
Brumertz <nbrandon165@gmail.com>
2025-02-07 14:34:28 +0800

    6 commit
    create motion button with the loop and vertical movement.

--- 3D-Portfolio/src/components/Hero.jsx
+++ 3D-Portfolio/src/components/Hero.jsx
@@ -4,13 +4,14 @@ import { motion } from 'framer-motion';
 
 import "../styles.css";
 import { ComputersCanvas } from './canvas';
-import { styles } from '../styles';
+
 
 
 const Hero = () => {
   return (
 <section className="relative w-full h-screen mx-auto">
-  <div className={`padding-x absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>  
+  <div className={`absolute inset-0 top-[90px] max-w-7xl mx-auto flex padding-x flex-row items-start gap-5`}
+  >  
     <div className=" flex flex-col items-center padding-x justify-center mt-5">
       <div className="w-5 h-5 rounded-full bg-[#915eff]"/>
       <div className="w-1 sm:h-80  h-40 violet-gradient"/>
@@ -20,11 +21,36 @@ const Hero = () => {
         <p className="hero-sub mt-2 text-white-100">I develop 3D visuals, user <br className="sm:block hidden "/> interfaces and web applications.</p>
     </div>
   </div>
+
   <ComputersCanvas />
+  <div className='absolute xs:bottom-10 bottom-14  w-full flex justify-center items-center
+  '>
+    <a href="#about">
+      <div 
+        className="w-4 h-9 rounded-3xl  flex justify-center items-start p-2"
+        style={{
+          border: "2px solid var(--color-secondary)",
+        }}
+      >
+        <motion.div
+          animate={{
+            y: [0, 24, 0],
+          }}
+          transition={{
+            duration: 1.5,
+            repeat: Infinity,
+            repeatType: "loop",
+          }}
+          className='w-2 h-2 rounded-full  mb-1'
+          style={{
+            backgroundColor: "var(--color-secondary)",
+          }}
+        />
+      </div>
+    </a>
+  </div>
 </section>
-
-
-  )
-}
+  );
+};
 
 export default Hero
--- 3D-Portfolio/src/components/canvas/Computers.jsx
+++ 3D-Portfolio/src/components/canvas/Computers.jsx
@@ -1,53 +1,54 @@
-import React, { Suspense, useEffect, useState } from "react";
-import { Canvas } from "@react-three/fiber";
+import React, { Suspense, useEffect, useState, useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import * as THREE from "three";
 
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
+  const spotLightRef = useRef();
+
+  useEffect(() => {
+    if (spotLightRef.current) {
+      spotLightRef.current.lookAt(new THREE.Vector3(0, 0, 0));
+      spotLightRef.current.shadow.mapSize.set(1024, 1024);
+    }
+  }, []);
 
   return (
     <mesh receiveShadow castShadow>   
-      <hemisphereLight intensity={.72} groundColor='[#0000]' />
-      <spotLight intensity={8} castShadow pointlightposition={[-20, 50, 10]} />
-      <pointLight intensity={3} />
+      <hemisphereLight intensity={1.5} groundColor={new THREE.Color("black")} />
+      <spotLight
+        ref={spotLightRef}
+        angle={0.3}
+        penumbra={1}
+        intensity={5}
+        castShadow
+      />
+      <pointLight intensity={2} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.5 : 0.73}
-        position={isMobile ? [0, -2, -1.8] : [0, -3.50, -1.5]}
+        scale={isMobile ? 0.5 : 0.75}
+        position={isMobile ? [0, -2, -1.6] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
+        castShadow
       />
     </mesh>
   );
 };
 
 const ComputersCanvas = () => {
-  const [isMobile, setIsMobile] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 500);
 
   useEffect(() => {
-    // Add a listener for changes to the screen size
-    const mediaQuery = window.matchMedia("(max-width: 500px)");
-
-    // Set the initial value of the `isMobile` state variable
-    setIsMobile(mediaQuery.matches);
-
-    // Define a callback function to handle changes to the media query
-    const handleMediaQueryChange = (event) => {
-      setIsMobile(event.matches);
-    };
-
-    // Add the callback function as a listener for changes to the media query
-    mediaQuery.addEventListener("change", handleMediaQueryChange);
-
-    // Remove the listener when the component is unmounted
-    return () => {
-      mediaQuery.removeEventListener("change", handleMediaQueryChange);
-    };
+    const handleResize = () => setIsMobile(window.innerWidth <= 500);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return (
-    <Canvas
+    <Canvas 
       frameloop='demand'
       shadows
       dpr={[1, 2]}
